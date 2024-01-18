73c03915e5276b1a8ffadd18f79dd42ed2977881
Manuel <nuelay99@gmail.com>
2024-01-18 16:17:31 +0100

    second commit

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -5,7 +5,7 @@ import Header from './Header'
 
 function Layout() {
   return (
-    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+    <div className="flex flex-row bg-neutral-100 h-full w-full ">
         <Sidebar/>
         <div className="flex flex-col flex-1">
             <Header/>
