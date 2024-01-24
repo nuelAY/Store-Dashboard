35f699a44879b07afd91d8f4ffc657b3bbc09dca
Manuel <nuelay99@gmail.com>
2024-01-24 01:55:38 +0100

    another update

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -83,7 +83,7 @@ function Header() {
                     leaveFrom="opacity-100 translate-y-0"
                     leaveTo="opacity-0 translate-y-1"
                     >
-                        <Popover.Panel className='absolute right-0 z-10 mt-2.5 w-80'>
+                        <Popover.Panel className='absolute right-0 z-10 mt-2.5 w-80 mx-[-4rem]'>
                                 <div className='bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5'>
                                       <strong className='text-gray-700 font-medium'>Notifications!</strong>
                                       <div className='mt-2 py-1 text-sm'>
